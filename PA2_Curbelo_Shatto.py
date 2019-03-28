5c84451e19680dc9dc9c91906dbc18ee3d44244f
gcurbelo123 <gcurbelo@csumb.edu>
2019-03-27 23:27:39 -0700

    Update PA2_Curbelo_Shatto.py

--- PA2_Curbelo_Shatto.py
+++ PA2_Curbelo_Shatto.py
@@ -7,39 +7,18 @@ from socket import *
 serverName = 'localhost'
 serverPort = 12000
 
-# Initialize text color - because we're extra
-green = "\033[92m"
-red = "\033[91m"
-yellow = "\033[33m"
-magenta = "\033[35m"
-bold = "\033[1m"
-end = "\033[0m"
+# Set up client socket
+clientSocket = socket(AF_INET,SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
 
-def setUpClient(name):
-  # Set up client socket
-  clientSocket = socket(AF_INET,SOCK_STREAM)
+msg = "Client X: Alice"
+clientSocket.send(msg.encode())
 
-  clientSocket.connect((serverName, serverPort))
-  msg = "Client " + name
-  clientSocket.send(msg.encode())
+msg = "Client Y: Bob"
+clientSocket.send(msg.encode())
 
-  print "\n" + green + "Client " + name + " message sent." + end + "\n"
-  
-  # Receive Server Message
-  newMsg = clientSocket.recv(1024).decode()
-  print newMsg
-  clientSocket.close()
+# Receive Server Message
+newMsg = clientSocket.recv(1024).decode()
+print newMsg
+clientSocket.close()
 
-  print "\n" + green + "Server message received." + end + "\n"
-
-def createThreads():
-  bobThread = Thread(target = setUpClient, args = ("Y: Bob",))
-  aliceThread = Thread(target = setUpClient, args = ("X: Alice",))
-  
-  print "\n" + magenta + "*TCP Simulator*" + end + "\n"
-  bobThread.start()
-  aliceThread.start()
-  bobThread.join()
-  aliceThread.join()
-
-createThreads()
